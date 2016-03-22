9f4a98a9cd1af15d84e7b4778ad9fc958789c551
Pavlin Poličar <pavlin.g.p@gmail.com>
2016-03-22 16:41:06 +0000

    Added behat for acceptance testing

---
+++ features/bootstrap/FeatureContext.php
@@ -0,0 +1,25 @@
+<?php
+
+use Behat\Behat\Context\Context;
+use Behat\Behat\Context\SnippetAcceptingContext;
+use Behat\MinkExtension\Context\MinkContext;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+/**
+ * Defines application features from the specific context.
+ */
+class FeatureContext extends MinkContext implements Context, SnippetAcceptingContext
+{
+    use DatabaseTransactions;
+
+    /**
+     * Initializes context.
+     *
+     * Every scenario gets its own context instance.
+     * You can also pass arbitrary arguments to the
+     * context constructor through behat.yml.
+     */
+    public function __construct()
+    {
+    }
+}
