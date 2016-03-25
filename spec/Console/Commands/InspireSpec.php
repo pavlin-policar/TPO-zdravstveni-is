0eb8246156e56911ff526523de24dac81829045e
Pavlin Poličar <pavlin.g.p@gmail.com>
2016-03-25 12:11:40 +0000

    Added laravel form helpers

--- spec/Console/Commands/InspireSpec.php
+++ spec/Console/Commands/InspireSpec.php
@@ -17,6 +17,6 @@ class InspireSpec extends LaravelObjectBehavior
 {
     public function it_is_initializable()
     {
-        $this->shouldHaveType('App\Console\Commands\Inspire');
+        $this->shouldBeAnInstanceOf('App\Console\Commands\Inspire');
     }
 }
