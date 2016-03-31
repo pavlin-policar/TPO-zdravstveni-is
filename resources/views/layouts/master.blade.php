e440b0b523cf23a62507451a4e03cc69774a3ed9
Pavlin Poličar <pavlin.g.p@gmail.com>
2016-03-31 22:48:20 +0200

    Renamed template asset folder to vendor and updated master template

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,14 +7,14 @@
     {{ Html::style('http://fonts.googleapis.com/css?family=Roboto+Condensed:300,400') }}
     {{ Html::style('http://fonts.googleapis.com/css?family=Lato:300,400,700,900') }}
     {{-- Vendor stylesheets --}}
-    {{ Html::style('lib/css/bootstrap.min.css') }}
-    {{ Html::style('lib/css/font-awesome.min.css') }}
-    {{ Html::style('lib/css/animate.min.css') }}
-    {{ Html::style('lib/css/bootstrap-switch.min.css') }}
-    {{ Html::style('lib/css/checkbox3.min.css') }}
-    {{ Html::style('lib/css/jquery.dataTables.min.css') }}
-    {{ Html::style('lib/css/dataTables.bootstrap.css') }}
-    {{ Html::style('lib/css/select2.min.css') }}
+    {{ Html::style('vendor/css/bootstrap.min.css') }}
+    {{ Html::style('vendor/css/font-awesome.min.css') }}
+    {{ Html::style('vendor/css/animate.min.css') }}
+    {{ Html::style('vendor/css/bootstrap-switch.min.css') }}
+    {{ Html::style('vendor/css/checkbox3.min.css') }}
+    {{ Html::style('vendor/css/jquery.dataTables.min.css') }}
+    {{ Html::style('vendor/css/dataTables.bootstrap.css') }}
+    {{ Html::style('vendor/css/select2.min.css') }}
     {{-- App stylesheets --}}
     {{ Html::style('css/style.css') }}
     {{ Html::style('css/themes/flat-blue.css') }}
@@ -36,17 +36,17 @@
     @include('partials.footer')
 </div>
 {{-- Vendor scripts --}}
-{{ Html::script('lib/js/jquery.min.js') }}
-{{ Html::script('lib/js/bootstrap.min.js') }}
-{{ Html::script('lib/js/Chart.min.js') }}
-{{ Html::script('lib/js/bootstrap-switch.min.js') }}
-{{ Html::script('lib/js/jquery.matchHeight-min.js') }}
-{{ Html::script('lib/js/jquery.dataTables.min.js') }}
-{{ Html::script('lib/js/dataTables.bootstrap.min.js') }}
-{{ Html::script('lib/js/select2.full.min.js') }}
-{{ Html::script('lib/js/ace/ace.js') }}
-{{ Html::script('lib/js/ace/mode-html.js') }}
-{{ Html::script('lib/js/ace/theme-github.js') }}
+{{ Html::script('vendor/js/jquery.min.js') }}
+{{ Html::script('vendor/js/bootstrap.min.js') }}
+{{ Html::script('vendor/js/Chart.min.js') }}
+{{ Html::script('vendor/js/bootstrap-switch.min.js') }}
+{{ Html::script('vendor/js/jquery.matchHeight-min.js') }}
+{{ Html::script('vendor/js/jquery.dataTables.min.js') }}
+{{ Html::script('vendor/js/dataTables.bootstrap.min.js') }}
+{{ Html::script('vendor/js/select2.full.min.js') }}
+{{ Html::script('vendor/js/ace/ace.js') }}
+{{ Html::script('vendor/js/ace/mode-html.js') }}
+{{ Html::script('vendor/js/ace/theme-github.js') }}
 {{-- App scripts --}}
 {{ Html::script('js/app.js') }}
 </body>
