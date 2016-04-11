81a9ba8802877d8dfd3d55d14b08e5c2f586ee69
Pavlin Poličar <pavlin.g.p@gmail.com>
2016-04-11 07:33:03 +0200

    Updated password validation rule

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -65,7 +65,7 @@ class AuthController extends Controller
     {
         return Validator::make($data, [
             'email' => 'required|email|max:255|unique:users',
-            'password' => 'required|min:6|confirmed',
+            'password' => 'required|min:8|confirmed',
         ]);
     }
 
