28b4bd5fcc6d2e1d19cf6cd92341f07c831fbccb
lavrinec <ml9987@student.uni-lj.si>
2016-04-14 19:39:15 +0200

    password reset fix

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'info@zis.si', 'name' => 'Zdravstveni informacijski sistem'],
 
     /*
     |--------------------------------------------------------------------------
