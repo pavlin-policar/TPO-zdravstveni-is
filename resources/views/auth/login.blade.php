04a90aa1316625ef22c4e46cc8d1c4d9d9f77322
Pavlin Poličar <pavlin.g.p@gmail.com>
2016-04-14 13:51:07 +0200

    Changed input form with a prettier version

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,66 +1,95 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html>
 
-@section('content')
+<head>
+    <title>Flat Admin V.2 - Free Bootstrap Admin Templates</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    {{-- Fonts --}}
+    {{ Html::style('http://fonts.googleapis.com/css?family=Roboto+Condensed:300,400') }}
+    {{ Html::style('http://fonts.googleapis.com/css?family=Lato:300,400,700,900') }}
+    {{-- Vendor stylesheets --}}
+    {{ Html::style('vendor/css/bootstrap.min.css') }}
+    {{ Html::style('vendor/css/font-awesome.min.css') }}
+    {{ Html::style('vendor/css/animate.min.css') }}
+    {{ Html::style('vendor/css/bootstrap-switch.min.css') }}
+    {{ Html::style('vendor/css/checkbox3.min.css') }}
+    {{ Html::style('vendor/css/jquery.dataTables.min.css') }}
+    {{ Html::style('vendor/css/dataTables.bootstrap.css') }}
+    {{ Html::style('vendor/css/select2.min.css') }}
+    {{-- App stylesheets --}}
+    {{ Html::style('css/style.css') }}
+    {{ Html::style('css/themes/flat-blue.css') }}
+</head>
+
+<body class="flat-blue login-page">
 <div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
-                        {!! csrf_field() !!}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label" for="email">E-Mail naslov</label>
-
-                            <div class="col-md-6">
-                                <input type="email" class="form-control" name="email" value="{{ old('email') }}" id="email" placeholder="Email">
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
+    <div class="login-box">
+        <div>
+            <div class="login-form row">
+                <div class="col-sm-12 text-center login-header">
+                    <i class="login-logo fa fa-heartbeat fa-5x"></i>
+                    <h4 class="login-title">Zdravstveni informacijski sistem</h4>
+                </div>
+                <div class="col-sm-12">
+                    <div class="login-body">
+                        <div class="progress hidden" id="login-progress">
+                            <div class="progress-bar progress-bar-success progress-bar-striped active" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">
+                                Log In...
+                            </div>
+                        </div>
+                        <form action="{{ url('/login') }}" method="post">
+                            {!! csrf_field() !!}
+                            <div class="control">
+                                <input type="email" name="email" class="form-control" placeholder="Elektronski naslov" value="{{ old('email') }}" />
+                            </div>
+                            <div class="control">
+                                <input type="password" name="password" class="form-control" placeholder="Geslo"/>
+                            </div>
+                            @if ($errors->has('email'))
+                                <span class="help-block">
+                                        {{ $errors->first('email') }}
                                     </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label" for="password">Geslo</label>
-
-                            <div class="col-md-6">
-                                <input type="password" class="form-control" name="password" id="password" placeholder="Geslo">
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
+                            @endif
+                            @if ($errors->has('password'))
+                                <span class="help-block">
+                                        {{ $errors->first('password') }}
                                     </span>
-                                @endif
+                            @endif
+                            <div class="checkbox text-left">
+                                <label>
+                                    <input type="checkbox" name="remember">Zapomni se me
+                                </label>
                             </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember"> Zapomni si me
-                                    </label>
-                                </div>
+                            <div class="login-button text-center">
+                                <input type="submit" class="btn btn-primary" value="Prijava">
                             </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-sign-in"></i>Prijava
-                                </button>
-
-                                <a class="btn btn-link" href="{{ url('/password/reset') }}">Pozabljeno geslo</a>
-                            </div>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
+                    <div class="login-footer">
+                        <span class="text-right"><a href="{{ url('/password/reset') }}" class="color-white">Pozabljeno geslo?</a></span>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
-@endsection
+{{-- Vendor scripts --}}
+{{ Html::script('vendor/js/jquery.min.js') }}
+{{ Html::script('vendor/js/bootstrap.min.js') }}
+{{ Html::script('vendor/js/Chart.min.js') }}
+{{ Html::script('vendor/js/bootstrap-switch.min.js') }}
+{{ Html::script('vendor/js/jquery.matchHeight-min.js') }}
+{{ Html::script('vendor/js/jquery.dataTables.min.js') }}
+{{ Html::script('vendor/js/dataTables.bootstrap.min.js') }}
+{{ Html::script('vendor/js/select2.full.min.js') }}
+{{ Html::script('vendor/js/ace/ace.js') }}
+{{ Html::script('vendor/js/ace/mode-html.js') }}
+{{ Html::script('vendor/js/ace/theme-github.js') }}
+{{ Html::script('vendor/js/bootstrap-timepicker.min.js') }}
+
+{{-- App scripts --}}
+{{ Html::script('js/app.js') }}
+{{ Html::script('js/zis-compiled.js') }}
+</body>
+
+</html>
