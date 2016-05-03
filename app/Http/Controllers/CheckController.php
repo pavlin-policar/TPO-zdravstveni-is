bdeb0b80dc7b6dacc1a658bdc9d4eac05dc265b6
urko94 <uk2897@student.uni-lj.si>
2016-05-03 16:41:05 +0200

    check if user is authorized

--- app/Http/Controllers/CheckController.php
+++ app/Http/Controllers/CheckController.php
@@ -36,55 +36,62 @@ class CheckController extends Controller
         return view('home');
     }
 
-    public function show($id)
-    {
-        $data['check'] = DB::table('checks')
-            ->join('users', 'checks.doctor', '=', 'users.id')
-            ->join('doctor_dates', 'checks.doctor_date', '=', 'doctor_dates.id')
-            ->select('doctor_dates.time', 'users.first_name', 'users.last_name')
-            ->where('checks.id', '=', $id)
-            ->first();
+    public function show($id) {
 
-        $data['checkData'] = DB::table('checks_codes')
-            ->join('checks', 'checks_codes.check', '=', 'checks.id')
-            ->join('codes', 'checks_codes.code', '=', 'codes.id')
-            ->select('checks_codes.*', 'codes.name', 'codes.description', 'codes.code_type')
-            ->where('checks.id', '=', $id)
-            ->get();
-        $data['checkCountMedical'] = DB::table('checks_codes')
-            ->join('checks', 'checks_codes.check', '=', 'checks.id')
-            ->join('codes', 'checks_codes.code', '=', 'codes.id')
-            ->select('checks_codes.id')
-            ->where('checks.id', $id)
-            ->where('codes.code_type', 14)
-            ->count();
-        $data['checkCountDisease'] = DB::table('checks_codes')
-            ->join('checks', 'checks_codes.check', '=', 'checks.id')
-            ->join('codes', 'checks_codes.code', '=', 'codes.id')
-            ->select('checks_codes.*')
-            ->where('checks.id', '=', $id)
-            ->where('codes.code_type', 13)
-            ->count();
-        $data['checkCountDiet'] = DB::table('checks_codes')
-            ->join('checks', 'checks_codes.check', '=', 'checks.id')
-            ->join('codes', 'checks_codes.code', '=', 'codes.id')
-            ->select('checks_codes.*')
-            ->where('checks.id', '=', $id)
-            ->where('codes.code_type', 12)
-            ->count();
+        $thisCheck = Checks::findOrFail($id);
+
+        if(($thisCheck->doctor == session('user'))||($thisCheck->patient == session('user'))||($thisCheck->patient == session('showUser'))) {
+            $data['check'] = DB::table('checks')
+                ->join('users', 'checks.doctor', '=', 'users.id')
+                ->join('doctor_dates', 'checks.doctor_date', '=', 'doctor_dates.id')
+                ->select('doctor_dates.time', 'users.first_name', 'users.last_name')
+                ->where('checks.id', '=', $id)
+                ->first();
+
+            $data['checkData'] = DB::table('checks_codes')
+                ->join('checks', 'checks_codes.check', '=', 'checks.id')
+                ->join('codes', 'checks_codes.code', '=', 'codes.id')
+                ->select('checks_codes.*', 'codes.name', 'codes.description', 'codes.code_type')
+                ->where('checks.id', '=', $id)
+                ->get();
+            $data['checkCountMedical'] = DB::table('checks_codes')
+                ->join('checks', 'checks_codes.check', '=', 'checks.id')
+                ->join('codes', 'checks_codes.code', '=', 'codes.id')
+                ->select('checks_codes.id')
+                ->where('checks.id', $id)
+                ->where('codes.code_type', 14)
+                ->count();
+            $data['checkCountDisease'] = DB::table('checks_codes')
+                ->join('checks', 'checks_codes.check', '=', 'checks.id')
+                ->join('codes', 'checks_codes.code', '=', 'codes.id')
+                ->select('checks_codes.*')
+                ->where('checks.id', '=', $id)
+                ->where('codes.code_type', 13)
+                ->count();
+            $data['checkCountDiet'] = DB::table('checks_codes')
+                ->join('checks', 'checks_codes.check', '=', 'checks.id')
+                ->join('codes', 'checks_codes.code', '=', 'codes.id')
+                ->select('checks_codes.*')
+                ->where('checks.id', '=', $id)
+                ->where('codes.code_type', 12)
+                ->count();
 
 
-        $data['checkMeasurement'] = DB::table('measurements')
-            ->join('codes', 'measurements.type', '=', 'codes.id')
-            ->join('code_types', 'code_types.id', '=', 'codes.code_type')
-            ->join('users', 'measurements.provider', '=', 'users.id')
-            ->join('checks', 'measurements.check', '=', 'checks.id')
-            ->join('measurement_results', 'measurements.id', '=', 'measurement_results.measurement')
-            ->select('measurements.*', 'users.first_name', 'users.last_name', 'checks.note', 'measurement_results.result', 'code_types.name')
-            ->where('checks.id', '=', $id)
-            ->get();
+            $data['checkMeasurement'] = DB::table('measurements')
+                ->join('codes', 'measurements.type', '=', 'codes.id')
+                ->join('code_types', 'code_types.id', '=', 'codes.code_type')
+                ->join('users', 'measurements.provider', '=', 'users.id')
+                ->join('checks', 'measurements.check', '=', 'checks.id')
+                ->join('measurement_results', 'measurements.id', '=', 'measurement_results.measurement')
+                ->select('measurements.*', 'users.first_name', 'users.last_name', 'checks.note', 'measurement_results.result', 'code_types.name')
+                ->where('checks.id', '=', $id)
+                ->get();
 
-        return view('checks.show')->with($data);
+            return view('checks.show')->with($data);
+        }
+        else {
+            return abort(403, 'Unauthorized action.');
+        }
     }
 
     public function showMedical(User $user)
@@ -132,28 +139,35 @@ class CheckController extends Controller
         return view('checks.diet')->with($data);
     }
 
-    public function doctorDate($id)
-    {
+    public function doctorDate($id){
+
         $data['dates'] =  DoctorDates::findOrFail($id);
-        $data['patient'] = User::find($data['dates']->patient);
-        $data['doctors'] = User::where('person_type', 4)->get();
-        $data['checks'] = Checks::where('doctor_date', $id)->get();
-        $data['codesMedical'] = Code::where('code_type', 14)->get();
-        $data['codesDisease'] = Code::where('code_type', 13)->get();
-        $data['codesDiet'] = Code::where('code_type', 12)->get();
 
-        if(count($data['checks']) > 0){
-            foreach ($data['checks'] as $check) {
-                $data['checkData'][$check->id] = DB::table('checks_codes')
-                    ->join('checks', 'checks_codes.check', '=', 'checks.id')
-                    ->join('codes', 'checks_codes.code', '=', 'codes.id')
-                    ->select('checks_codes.*', 'codes.name', 'codes.description', 'codes.code_type')
-                    ->where('checks.id', '=', $check->id)
-                    ->get();
+        if($data['dates']->doctor == session('user')) {
+
+            $data['patient'] = User::find($data['dates']->patient);
+            $data['doctors'] = User::where('person_type', 4)->get();
+            $data['checks'] = Checks::where('doctor_date', $id)->get();
+            $data['codesMedical'] = Code::where('code_type', 14)->get();
+            $data['codesDisease'] = Code::where('code_type', 13)->get();
+            $data['codesDiet'] = Code::where('code_type', 12)->get();
+
+            if (count($data['checks']) > 0) {
+                foreach ($data['checks'] as $check) {
+                    $data['checkData'][$check->id] = DB::table('checks_codes')
+                        ->join('checks', 'checks_codes.check', '=', 'checks.id')
+                        ->join('codes', 'checks_codes.code', '=', 'codes.id')
+                        ->select('checks_codes.*', 'codes.name', 'codes.description', 'codes.code_type')
+                        ->where('checks.id', '=', $check->id)
+                        ->get();
+                }
             }
-        }
 
-        return view('checks.doctor')->with($data);
+            return view('checks.doctor')->with($data);
+        }
+        else{
+            return abort(403, 'Unauthorized action.');
+        }
     }
 
     public function checkUpdate(CheckRequest $request, $id)
